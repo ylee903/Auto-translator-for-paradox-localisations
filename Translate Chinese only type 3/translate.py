4d5c1c563a04ef828d42c8e20bc1854c0020d799
ylee903 <79842277+ylee903@users.noreply.github.com>
2024-08-30 01:48:19 +1200

    updated regex

--- Translate Chinese only type 3/translate.py
+++ Translate Chinese only type 3/translate.py
@@ -36,7 +36,7 @@ openai.api_key = api_key
 encoding = tiktoken.encoding_for_model(model_name)
 
 # Regular expression for extracting Chinese text
-chinese_text_regex = r'"([\u4e00-\u9fff]+[^"]*)"'
+chinese_text_regex = r'"([^"]*[\u4e00-\u9fff][^"]*)"'
 
 # Define token and line limits
 TOKEN_LIMIT = 3500  # Adjust if necessary for other models
